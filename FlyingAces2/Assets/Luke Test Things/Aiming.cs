6bbf7cc50b487356423318c9fa67fd05f86cc33c
jason-marks <44442842+jason-marks@users.noreply.github.com>
2020-02-06 15:05:00 -0600

    Beginnings of improved aiming

---
+++ FlyingAces2/Assets/Luke Test Things/Aiming.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Aiming : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField]
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //cam = GetComponentInChildren<Camera>();
+        Cursor.visible = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AimLogic();
+    }
+
+    void AimLogic()
+    {
+        float leftrightvalue = Input.GetAxisRaw("Mouse X");
+        float updownvalue = Input.GetAxisRaw("Mouse Y");
+        //Vector3 rotationX = new Vector3(-updownvalue, 0, 0);
+        //Vector3 rotationY = new Vector3(0, leftrightvalue, 0);
+
+        gameObject.transform.Rotate(-updownvalue, leftrightvalue, 0);
+
+        //rb.MoveRotation(rb.rotation * Quaternion.Euler(rotationY));
+        //cam.transform.Rotate(rotationX / 3);
+    }
+}
