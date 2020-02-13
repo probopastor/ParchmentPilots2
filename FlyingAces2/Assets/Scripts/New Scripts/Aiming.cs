6e79378c8ba2d578013d4a2087fd0387d5770139
grantfreyBU <54907819+grantfreyBU@users.noreply.github.com>
2020-02-13 14:53:24 -0600

    Refactored AimScript into Aiming

---
+++ FlyingAces2/Assets/Scripts/New Scripts/Aiming.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Aiming : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [Tooltip("How quickly the camera reacts to mouse movement")]
+    public float mouseSensitivity = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AimLogic();
+    }
+
+    void AimLogic()
+    {
+        // Input value for vertical mouse movement
+        float leftrightvalue = Input.GetAxisRaw("Mouse X");
+        // Input value for horizontal mouse movement
+        float updownvalue = Input.GetAxisRaw("Mouse Y");
+
+        // The Vector3 for rotating the player vertically
+        Vector3 rotationX = new Vector3(-updownvalue, 0, 0);
+        // The Vector3 for rotating the player horizontally
+        Vector3 rotationY = new Vector3(0, leftrightvalue, 0);
+
+        rb.transform.Rotate(rotationX / 3);
+        //rb.transform.Rotate(rotationY / 3);
+    }
+}
