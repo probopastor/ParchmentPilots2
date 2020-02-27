2318be5f631d39f96ea97f5391c853076b921716
grantfreyBU <54907819+grantfreyBU@users.noreply.github.com>
2020-02-27 13:46:52 -0600

    Added some functionallity to the main menu

---
+++ FlyingAces2/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainMenuController : MonoBehaviour
+{
+    GameObject[] mainMenu;
+    EventSystem eventSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu = GameObject.FindGameObjectsWithTag("MainMenu");
+        eventSystem = EventSystem.current;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenMainMenu()
+    {
+        foreach(GameObject button in mainMenu)
+        {
+            button.SetActive(true);
+        }
+
+    }
+
+    public void OpenLevelSelect()
+    {
+        foreach (GameObject button in mainMenu)
+        {
+            button.SetActive(false);
+        }
+    }
+
+    public void OpenCredits()
+    {
+        foreach (GameObject button in mainMenu)
+        {
+            button.SetActive(false);
+        }
+    }
+
+    public void OpenOptions()
+    {
+        foreach (GameObject button in mainMenu)
+        {
+            button.SetActive(false);
+        }
+    }
+
+    public void OpenHowToPlay()
+    {
+        foreach (GameObject button in mainMenu)
+        {
+            button.SetActive(false);
+        }
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+      Application.Quit();
+#endif
+    }
+}
