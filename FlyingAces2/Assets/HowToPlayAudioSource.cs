cc36ba0a24e143b51cb63ed66b604dbe45645e91
probopastor <43503592+probopastor@users.noreply.github.com>
2020-04-27 16:47:22 -0500

    e

---
+++ FlyingAces2/Assets/HowToPlayAudioSource.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HowToPlayAudioSource : MonoBehaviour
+{
+    public static HowToPlayAudioSource instance;
+
+    public string[] scenesToExistIn;
+    private bool keepAudioSource;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        keepAudioSource = false;
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+
+        string currentScene = SceneManager.GetActiveScene().name;
+
+        for (int i = 0; i < scenesToExistIn.Length; i++)
+        {
+            if(currentScene == scenesToExistIn[i])
+            {
+                keepAudioSource = true;
+            }
+        }
+
+        if(!keepAudioSource)
+        {
+            Destroy(this);
+        }
+    }
+}
