195d719b1c7d4d6ad6de74d3a550c9178cc656cd
AnnabellePries <annabellepries@sbcglobal.net>
2020-09-24 18:57:29 -0500

    Added new toaster animation.

---
+++ FlyingAces2/Assets/Animations/Toaster/ToasterAnimControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToasterAnimControl : MonoBehaviour
+{
+    public new Animation animation;
+
+    void Start()
+    {
+        animation = GetComponent<Animation>();
+        StartCoroutine(MyCoroutine());
+    }
+
+    void RepeatMyCoroutine()
+    {
+        StartCoroutine(MyCoroutine());
+    }
+
+    private IEnumerator MyCoroutine()
+    {
+        animation.Play("Toaster");
+        yield return new WaitForSeconds(5f);
+        animation.Stop("Toaster");
+        RepeatMyCoroutine();
+        yield return null;
+    }
+}
